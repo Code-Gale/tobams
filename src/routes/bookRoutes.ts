6d63c2417229b9e58a03d93eecbcb386c72272e1
Code-Gale <codewhiz001@gmail.com>
2024-08-01 12:38:27 +0100

    corrected cover to coverImage

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -9,6 +9,6 @@ router.get('/books', getBooks)
 router.get('/books/:id', getBookById)
 router.put('/books/:id', updateBook)
 router.delete('/books/:id', deleteBook)
-router.patch('/books/:id', upload.single('cover'), changeCover)
+router.patch('/books/:id', upload.single('coverImage'), changeCover)
 
 export default router;
