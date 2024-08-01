03fd7d8bfbe44549fe77e7c7ee27babe32310d31
Code-Gale <codewhiz001@gmail.com>
2024-08-01 09:54:11 +0100

    schema and model for Book

---
+++ src/models/Books.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+import { convertCompilerOptionsFromJson } from 'typescript'
+const { Schema, Document } = mongoose
+
+export interface IBook extends Document {
+    title: string,
+    author: string,
+    publishedDate : string,
+    isbn : string,
+    coverImage? : string
+}
+
+const bookSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    publishedDate: {
+        type: String,
+        required: true
+    },
+    isbn: {
+        type: String,
+        required: true
+    },
+    converImage: {
+        type: String
+    }
+})
+
+const Book = mongoose.model<IBook>('Book', bookSchema)
+
+module.exports = Book
