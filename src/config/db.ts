b391a5b4f7e85a7d5a33952010f89a99cbfdcde5
Code-Gale <codewhiz001@gmail.com>
2024-08-01 09:53:41 +0100

    database config file

---
+++ src/config/db.ts
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const connectDB = async (): Promise<void> => {
+    try{
+        await mongoose.connect(process.env.DBURI)
+        console.log('Connected to Database...')
+    }catch(err){
+        console.log('Could not connect to Database...', err)
+        process.exit(2)
+    }
+}
+
+module.exports = connectDB
